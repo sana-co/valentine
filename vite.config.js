3949bec819656afd65401bff66f288d34f699f07
sana-co <sanuthpvt@gmail.com>
2026-02-01 22:56:46 +0530

    repo match

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine/',   // 👈 MUST match repo name
-})
+  base: "/valentine/",
+});
+
